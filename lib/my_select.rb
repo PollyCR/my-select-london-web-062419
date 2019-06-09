02279e74a90653026a4449009907aeb393605cfd
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 20:20:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,3 +8,15 @@ return selection
 end 
 end 
 end
+
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
+end
