94a5d6a28aaa2d930299e5d6ec2662a9bc193832
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 20:20:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,13 +1,11 @@
 def my_select(collection)
-i = 0 
-selection = []
-while i > collection.length 
-if yield(collection[i])
-  selection << collection []
-i+= 1 
-selection 
-end 
-end 
+  i = 0
+  selection = []
+  while i < array.length
+    if yield(array[i])
+      selection << array[i]
+    end
+    i+=1
+  end
+  selection
 end
-
-
